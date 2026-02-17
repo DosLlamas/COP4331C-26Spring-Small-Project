426e737d18dd6f4626443319cbf5ffcfb904d981
Travis Burns <burns.travis.j@gmail.com>
2026-02-17 17:38:32 -0500

    Add contacts to nav bar for signed in users

--- client/js/code.js
+++ client/js/code.js
@@ -725,28 +725,7 @@ function showContactsNavPage() {
         return;
     }
 
-    userId = -1;
-    let data = document.cookie;
-    let splits = data.split(",");
-    for(var i = 0; i < splits.length; i++)
-    {
-        let thisOne = splits[i].trim();
-        let tokens = thisOne.split("=");
-        if( tokens[0] == "firstName" )
-        {
-            firstName = tokens[1];
-        }
-        else if( tokens[0] == "lastName" )
-        {
-            lastName = tokens[1];
-        }
-        else if( tokens[0] == "userId" )
-        {
-            userId = parseInt( tokens[1].trim() );
-        }
-    }
-
-    if (userId >= 0) {
+    if (userId > 0) {
         const navContainer = document.querySelector(".pages");
         if (navContainer) {
             const newLink = document.createElement("a");
